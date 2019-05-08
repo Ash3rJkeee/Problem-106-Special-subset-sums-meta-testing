b8b97c6a644eb075081639e68e3e6183dd09bd2d
Ash3rJkeee <aryadinskikh@gmail.com>
2019-05-08 17:05:06 +0300

    Something new

--- MainFile.py
+++ MainFile.py
@@ -2,39 +2,45 @@
 
 import itertools
 
-a4 = [3, 5, 6, 7]
+a = [3, 5, 6, 7]
 
 combinations = set()
 pair_combinations = list()
 
-for i in range(1, len(a4)):
-    combinations.update(itertools.combinations(a4, i))
+for b in range(1, len(a)):
+    combinations.update(itertools.combinations(a, b))
 
 
 combinations = list(combinations)
 combinations.sort()
 num = 0
 
-for i in combinations:
+for b in combinations:
     b_ = list(combinations)
-    b_.remove(i)
-    for j in b_:
-        sum_i = sum(i)
-        sum_j = sum(j)
-        len_set_i = len(list(i))
-        len_set_j = len(list(j))
+    b_.remove(b)
+    for с in b_:
+        sum_i = sum(b)
+        sum_j = sum(с)
+        len_set_b = len(list(b))
+        len_set_c = len(list(с))
 
-        set_i = set(i)
-        set_j = set(j)
+        set_b = set(b)
+        set_c = set(с)
 
         # определяем непересекающиеся подмножества и проводим над ними проверку по условию
-        if set_i.isdisjoint(set_j):
-            if len_set_j == len_set_i:
-                # проверка, чтобы не дублировать пары
-                if [j, i] not in pair_combinations:
-                    pair_combinations.append([i, j])
+        if set_b.isdisjoint(set_c):
+            # проверка, чтобы не дублировать пары
+            if [с, b] not in pair_combinations:
+                if (len_set_b > 1) and (len_set_c > 1):
+                    if len_set_b == len_set_c:
+                        pair_combinations.append([b, с])
 
 num = 0
-for i in pair_combinations:
-    print(num, i)
+for pair in pair_combinations:
+    print(num, pair)
     num = num + 1
+
+
+# a4 = [3, 5, 6, 7]
+# a7 = [20, 31, 38, 39, 40, 42, 45]
+
