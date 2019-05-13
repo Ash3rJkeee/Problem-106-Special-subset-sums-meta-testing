5ec8b6c27b91159095ef06ee95b03a1e88664d7a
Ash3rJkeee <aryadinskikh@gmail.com>
2019-05-13 21:43:22 +0300

    Начата разработка функции попарного сравнения множества. Пока что работает не идеально.

--- MainFile.py
+++ MainFile.py
@@ -2,7 +2,29 @@
 
 import itertools
 
-a = [3, 5, 6, 7]
+
+def pairs_comparison(b, c):
+    """попарно сравнивает множества между собой"""
+    if b[0] > c[0]:
+        if len(b) == 2:
+            return True
+        if c[-1] < b[0]:
+            return False
+        for i in range(1, len(b)):
+            if b[i] < c[i]:
+                return False
+    if b[0] < c[0]:
+        if len(b) == 1:
+            return True
+        if c[-1] > b[0]:
+            return False
+        for i in range(1, len(b)):
+            if b[i] > c[i]:
+                return False
+    return True
+
+
+a = [20, 31, 38, 39, 40, 42, 45]
 
 combinations = set()
 pair_combinations = list()
@@ -16,24 +38,25 @@ combinations.sort()
 num = 0
 
 for b in combinations:
-    b_ = list(combinations)
-    b_.remove(b)
-    for с in b_:
+    c_ = list(combinations)
+    c_.remove(b)
+    for c in c_:
         sum_i = sum(b)
-        sum_j = sum(с)
+        sum_j = sum(c)
         len_set_b = len(list(b))
-        len_set_c = len(list(с))
+        len_set_c = len(list(c))
 
         set_b = set(b)
-        set_c = set(с)
+        set_c = set(c)
 
         # определяем непересекающиеся подмножества и проводим над ними проверку по условию
         if set_b.isdisjoint(set_c):
             # проверка, чтобы не дублировать пары
-            if [с, b] not in pair_combinations:
+            if [c, b] not in pair_combinations:
                 if (len_set_b > 1) and (len_set_c > 1):
                     if len_set_b == len_set_c:
-                        pair_combinations.append([b, с])
+                        if pairs_comparison(b, c):
+                            pair_combinations.append([b, c])
 
 num = 0
 for pair in pair_combinations:
