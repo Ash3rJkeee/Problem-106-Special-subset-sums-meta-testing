9581aae33406c6044e2c0d69f6ca2e2c7f0ca5fc
Ash3rJkeee <aryadinskikh@gmail.com>
2019-05-16 18:00:28 +0300

    Дописаны todo метки.

--- MainFile.py
+++ MainFile.py
@@ -1,57 +1,69 @@
 """Условие задачи http://euler.jakumo.org/problems/view/106.html"""
 
+# todo 1) Написать функцию проверки пар подмножеств на удовлетворение условию. ГОТОВО
+# todo 2) Найти оптимальное А для n=12.
+# todo 3) Переписать функцию уточнения (оптимизации) А для n=12 или для неограниченного количества n.
+
 import itertools
 
 
-def pairs_comparison(b, c):
-    """попарно сравнивает множества между собой"""
-    if b[0] > c[0]:
-        if len(b) == 1:
-            return True
-        for i in range(1, len(b)):
-            if b[i] < c[i]:
+def find_pairs(input):
+    def pairs_comparison(b, c):
+        """попарно сравнивает множества между собой"""
+        if b[0] > c[0]:
+            if len(b) == 1:
                 return True
-    if b[0] < c[0]:
-        if len(b) == 1:
-            return True
-        for i in range(1, len(b)):
-            if b[i] > c[i]:
+            for i in range(1, len(b)):
+                if b[i] < c[i]:
+                    return True
+        if b[0] < c[0]:
+            if len(b) == 1:
                 return True
+            for i in range(1, len(b)):
+                if b[i] > c[i]:
+                    return True
+
+    a = input
+
+    combinations = set()
+    pair_combinations = list()
+
+    for b in range(1, len(a)):
+        combinations.update(itertools.combinations(a, b))
+
+    combinations = list(combinations)
+    combinations.sort()
+    num = 0
+
+    for b in combinations:
+        c_ = list(combinations)
+        c_.remove(b)
+        for c in c_:
+            sum_i = sum(b)
+            sum_j = sum(c)
+            len_set_b = len(list(b))
+            len_set_c = len(list(c))
+
+            set_b = set(b)
+            set_c = set(c)
+
+            # определяем непересекающиеся подмножества и проводим над ними проверку по условию
+            if set_b.isdisjoint(set_c):
+                # проверка, чтобы не дублировать пары
+                if [c, b] not in pair_combinations:
+                    if (len_set_b > 1) and (len_set_c > 1):
+                        if len_set_b == len_set_c:
+                            if pairs_comparison(b, c):
+                                pair_combinations.append([b, c])
+    return pair_combinations
+
+
+
 
 
 a = [20, 31, 38, 39, 40, 42, 45]
+pair_combinations = find_pairs(a)
 
-combinations = set()
-pair_combinations = list()
-
-for b in range(1, len(a)):
-    combinations.update(itertools.combinations(a, b))
-
-
-combinations = list(combinations)
-combinations.sort()
-num = 0
-
-for b in combinations:
-    c_ = list(combinations)
-    c_.remove(b)
-    for c in c_:
-        sum_i = sum(b)
-        sum_j = sum(c)
-        len_set_b = len(list(b))
-        len_set_c = len(list(c))
-
-        set_b = set(b)
-        set_c = set(c)
-
-        # определяем непересекающиеся подмножества и проводим над ними проверку по условию
-        if set_b.isdisjoint(set_c):
-            # проверка, чтобы не дублировать пары
-            if [c, b] not in pair_combinations:
-                if (len_set_b > 1) and (len_set_c > 1):
-                    if len_set_b == len_set_c:
-                        if pairs_comparison(b, c):
-                            pair_combinations.append([b, c])
 
 num = 0
 for pair in pair_combinations:
